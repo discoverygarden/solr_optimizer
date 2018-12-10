8c98dce990749b1f4f708de2c778cc268561eb8b
ackiejoe <alex@discoverygarden.ca>
2018-12-10 15:12:16 -0400

    adding double quotes around user:pass to allow for weird passwords

--- solr_reindex/reindex_a_pid.sh
+++ solr_reindex/reindex_a_pid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MESSAGE="Re-indexing $4"
-HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" -u $2:$3 "$1?operation=updateIndex&action=fromPid&value=$4")
+HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" -u "$2:$3" "$1?operation=updateIndex&action=fromPid&value=$4")
 
 if [ ! $HTTP_STATUS -eq 200  ]; then
   echo -e "ERROR $MESSAGE [HTTP status: $HTTP_STATUS]"
